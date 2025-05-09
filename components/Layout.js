5a4b121cc0eb2c3680002d7f1db669cff71bfdbc
jrg06009 <broncos262001@yahoo.com>
2025-05-09 15:14:50 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -18,17 +18,23 @@ export default function Layout({ children }) {
     <div className="min-h-screen bg-white text-black">
       <header className="bg-gray-900 text-white shadow-md">
         <div className="max-w-7xl mx-auto px-4 py-4 flex items-center">
-          <Link href="/" className="mr-6">
-            <img src="/logos/league.png" alt="Logo" className="h-10 w-auto" />
+          <Link href="/" passHref>
+            <a className="mr-6">
+              <img src="/logos/league.png" alt="Logo" className="h-10 w-auto" />
+            </a>
           </Link>
           <nav className="flex space-x-4">
             {navItems.map((item) => (
-              <Link
-                key={item.path}
-                href={item.path}
-                className={`${router.pathname.startsWith(item.path) ? 'text-yellow-400 font-semibold' : 'text-white hover:text-yellow-300'} px-2`}
-              >
-                {item.name}
+              <Link key={item.path} href={item.path} passHref>
+                <a
+                  className={`px-2 ${
+                    router.pathname.startsWith(item.path)
+                      ? 'text-yellow-400 font-semibold'
+                      : 'text-white hover:text-yellow-300'
+                  }`}
+                >
+                  {item.name}
+                </a>
               </Link>
             ))}
           </nav>
