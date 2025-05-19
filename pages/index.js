6588c92ce464f4429b8b06f01600ead9ebe6ce8c
jrg06009 <broncos262001@yahoo.com>
2025-05-18 20:49:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,11 +39,11 @@ export default function Home({ games, players, teams, standings }) {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
           <h3 className="text-xl font-semibold mb-2">American League</h3>
-          <StandingsTable standings={{ AL: standings.AL }} teams={teams} useFullName={false} />
+          <StandingsTable standings={{ AL: standings.AL }} teams={teams} useFullName={false} hideLeagueHeaders={true} />
         </div>
         <div>
           <h3 className="text-xl font-semibold mb-2">National League</h3>
-          <StandingsTable standings={{ NL: standings.NL }} teams={teams} useFullName={false} />
+          <StandingsTable standings={{ NL: standings.NL }} teams={teams} useFullName={false} hideLeagueHeaders={true} />
         </div>
       </div>
 
