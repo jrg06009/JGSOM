e7d1007b23a6f1d69392e1c94b8923c2de90a9bc
jrg06009 <broncos262001@yahoo.com>
2025-05-19 22:03:22 -0400

    Update [abbr].js

--- pages/teams/[abbr].js
+++ pages/teams/[abbr].js
@@ -21,6 +21,10 @@ export async function getStaticProps({ params }) {
   }
 }
 
+const sumStat = (arr, key) => arr.reduce((sum, p) => sum + (parseFloat(p[key]) || 0), 0)
+const formatPct = (num) => (num === 1 ? '1.000' : num ? num.toFixed(3).slice(1) : '')
+const formatRate = (num) => (num === '' || isNaN(num)) ? '' : parseFloat(num).toFixed(2)
+
 const TeamPage = ({ abbr, team }) => {
   if (!team) return <div className="p-4 text-red-600">Team not found.</div>
 
@@ -28,10 +32,23 @@ const TeamPage = ({ abbr, team }) => {
   const pStats = pitching.filter(p => p.team === abbr)
   const fStats = fielding.filter(p => p.team === abbr)
 
+  const battingKeys = [
+    'G','PA','AB','R','H','2B','3B','HR','RBI','SB','CS','BB','IBB','SO','AVG','OBP','SLG','OPS','TB','GDP','HBP','SH','SF'
+  ]
+
+  const pitchingKeys = [
+    'W','L','W-L%','ERA','G','GS','CG','SHO','SV','IP','H','R','ER','HR','BB','IBB','SO','HBP','BK','WP','H9','HR9','BB9','SO9','SO/BB'
+  ]
+
+  const fieldingKeys = [
+    'G','GS','CG','Inn','Ch','PO','A','E','DP','Fld%','PB','WP','SB','CS','CS%','PkO'
+  ]
+
+
   return (
     <div className="p-4">
       <div className="flex items-center mb-6">
-        <img src={team.logo} alt={team.name} className="h-12 mr-4" />
+        <img src={team.logo} alt={team.name} className="h-14 mr-4" />
         <h1 className="text-2xl font-bold" style={{ color: team.color }}>{team.name}</h1>
       </div>
       <SortableTable
@@ -39,18 +56,21 @@ const TeamPage = ({ abbr, team }) => {
         data={bStats}
         defaultSortKey="PA"
         exclude={['team']}
+        columns={battingKeys}          
       />
       <SortableTable
         title="Pitching"
         data={pStats}
         defaultSortKey="IP"
         exclude={['team']}
+        columns={pitchingKeys}          
       />
       <SortableTable
         title="Fielding"
         data={fStats}
         defaultSortKey="G"
         exclude={['team']}
+        columns={fieldingKeys}          
       />
     </div>
   )
