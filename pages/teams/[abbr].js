d928b16ef8a14bd54828e6cecb3607d6060ab253
jrg06009 <broncos262001@yahoo.com>
2025-05-19 22:11:02 -0400

    Update [abbr].js

--- pages/teams/[abbr].js
+++ pages/teams/[abbr].js
@@ -25,32 +25,6 @@ const sumStat = (arr, key) => arr.reduce((sum, p) => sum + (parseFloat(p[key]) |
 const formatPct = (num) => (num === 1 ? '1.000' : num ? num.toFixed(3).slice(1) : '')
 const formatRate = (num) => (num === '' || isNaN(num)) ? '' : parseFloat(num).toFixed(2)
 
-const getBattingTotal = (players) => {
-  const total = {}
-  const keys = ['H','AB','BB','HBP','SF','TB']
-  keys.forEach(k => total[k] = sumStat(players, k))
-  total.AVG = formatPct(total.H / total.AB)
-  total.OBP = formatPct((total.H + total.BB + total.HBP) / (total.AB + total.BB + total.HBP + total.SF))
-  total.SLG = formatPct(total.TB / total.AB)
-  total.OPS = formatPct(((total.H + total.BB + total.HBP) / (total.AB + total.BB + total.HBP + total.SF)) + (total.TB / total.AB))
-  return total
-}
-
-const getPitchingTotal = (players) => {
-  const total = {}
-  const keys = ['W','L','ER','IP','H','HR','BB','SO']
-  keys.forEach(k => total[k] = sumStat(players, k))
-  const ip = parseFloat(total.IP)
-  total['W-L%'] = formatPct(total.L + total.W > 0 ? total.W / (total.W + total.L) : 0)
-  total.ERA = ip ? (9 * total.ER / ip).toFixed(2) : ''
-  total.H9 = ip ? (9 * total.H / ip).toFixed(1) : ''
-  total.HR9 = ip ? (9 * total.HR / ip).toFixed(1) : ''
-  total.BB9 = ip ? (9 * total.BB / ip).toFixed(1) : ''
-  total.SO9 = ip ? (9 * total.SO / ip).toFixed(1) : ''
-  total['SO/BB'] = total.BB ? (total.SO / total.BB).toFixed(1) : ''
-  return total
-}
-
 const TeamPage = ({ abbr, team }) => {
   if (!team) return <div className="p-4 text-red-600">Team not found.</div>
 
@@ -59,12 +33,28 @@ const TeamPage = ({ abbr, team }) => {
   const fStats = fielding.filter(p => p.team === abbr)
 
   const battingKeys = [
-    'G','PA','AB','R','H','2B','3B','HR','RBI','SB','CS','BB','IBB','SO','AVG','OBP','SLG','OPS','TB','GDP','HBP','SH','SF'
+    'G','PA','AB','R','H','2B','3B','HR','RBI','SB','CS','BB','IBB','SO',
+    'AVG','OBP','SLG','OPS','TB','GDP','HBP','SH','SF'
   ]
+  const battingFormats = {
+    AVG: formatPct,
+    OBP: formatPct,
+    SLG: formatPct,
+    OPS: formatPct
+  }
 
   const pitchingKeys = [
     'W','L','W-L%','ERA','G','GS','CG','SHO','SV','IP','H','R','ER','HR','BB','IBB','SO','HBP','BK','WP','H9','HR9','BB9','SO9','SO/BB'
   ]
+  const pitchingFormats = {
+    'ERA': formatRate,
+    'W-L%': formatPct,
+    'H9': formatRate,
+    'HR9': formatRate,
+    'BB9': formatRate,
+    'SO9': formatRate,
+    'SO/BB': formatRate
+  }
 
   const fieldingKeys = [
     'G','GS','CG','Inn','Ch','PO','A','E','DP','Fld%','PB','WP','SB','CS','CS%','PkO'
@@ -82,7 +72,8 @@ const TeamPage = ({ abbr, team }) => {
         defaultSortKey="PA"
         exclude={['team']}
         columns={battingKeys}
-        totalsFn={getBattingTotal}
+        columnFormats={battingFormats}
+        showTotals={true}
       />
       <SortableTable
         title="Pitching"
@@ -90,7 +81,8 @@ const TeamPage = ({ abbr, team }) => {
         defaultSortKey="IP"
         exclude={['team']}
         columns={pitchingKeys}
-        totalsFn={getPitchingTotal}
+        columnFormats={pitchingFormats}
+        showTotals={true}
       />
       <SortableTable
         title="Fielding"
@@ -98,6 +90,7 @@ const TeamPage = ({ abbr, team }) => {
         defaultSortKey="G"
         exclude={['team']}
         columns={fieldingKeys}
+        showTotals={true}
       />
     </div>
   )
