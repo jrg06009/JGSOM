012d61303563ecd825a1844bd7070fc56eb4dadc
jrg06009 <broncos262001@yahoo.com>
2025-05-29 00:30:50 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,11 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 
 
-<div className="w-full max-w-screen px-4 sm:px-6 lg:px-8">
-  export default function App({ Component, pageProps }) {
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    )
-  }
-</div>
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
