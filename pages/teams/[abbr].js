73bb579ad21b65a447fce9cd3a29a7965fa9629a
jrg06009 <broncos262001@yahoo.com>
2025-05-29 08:03:39 -0400

    Update [abbr].js

--- pages/teams/[abbr].js
+++ pages/teams/[abbr].js
@@ -81,7 +81,7 @@ const TeamPage = ({ abbr, team }) => {
 
     return (
       <div className="mb-8">
-        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+        <div className="overflow-x-auto"><h2 className="text-xl font-semibold mb-2">{title}</h2>
         <table className="w-full text-sm border border-collapse">
           <thead>
             <tr className="bg-gray-100">
